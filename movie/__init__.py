92c5b0a000fb28620391fdc00f66c0c5e67c5a6e
chistoiy <chistoiy@outlook.com>
2019-01-25 22:53:58 +0800

    '后端页面布局完成'

--- movie/__init__.py
+++ movie/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask,render_template
-from movie.home import home
+from movie.home import home as home_blueprint
+from movie.admin import admin as admin_blueprint
 def create_app():
     app = Flask(__name__)
     app.debug=True
-    app.register_blueprint(home)
+    app.register_blueprint(home_blueprint)
+    app.register_blueprint(admin_blueprint,url_prefix='/admin')
 
     @app.errorhandler(404)
     def page_not_found(error):
